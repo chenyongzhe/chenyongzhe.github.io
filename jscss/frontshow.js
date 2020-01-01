ee3059a6ad9aa4ff9e768bfaf1181d6ea0327408
陈永喆 <34902854+chenyongzhe@users.noreply.github.com>
2020-01-01 10:35:37 +0800

    Add files via upload

---
+++ jscss/frontshow.js
@@ -0,0 +1,47 @@
+var message="LXY，我喜欢你，直到永远。"
+var neonbasecolor="white"
+var neontextcolor="red"
+var flashspeed=100  //in milliseconds
+var n=0
+if (document.all||document.getElementById){
+document.write('<font color="'+neonbasecolor+'">')
+for (m=0;m<message.length;m++)
+document.write('<span id="neonlight'+m+'">'+message.charAt(m)+'</span>')
+document.write('</font>')
+}
+else
+document.write(message)
+function crossref(number){
+var crossobj=document.all? eval("document.all.neonlight"+number) : document.getElementById("neonlight"+number)
+return crossobj
+}
+function neon(){
+//Change all letters to base color
+if (n==0){
+for (m=0;m<message.length;m++)
+//eval("document.all.neonlight"+m).style.color=neonbasecolor
+crossref(m).style.color=neonbasecolor
+}
+//cycle through and change individual letters to neon color
+crossref(n).style.color=neontextcolor
+if (n<message.length-1)
+n++
+else{
+n=0
+clearInterval(flashing)
+setTimeout("beginneon()",1500)
+return
+}
+}
+
+
+function mystart(){
+    setTimeout(beginneon,22500);
+}
+function beginneon(){
+$(".hide").removeClass('hide');
+if (document.all||document.getElementById)
+flashing=setInterval("neon()",flashspeed)
+}
+
+mystart();
